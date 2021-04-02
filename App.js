95fbe63f1bf75ca3d915f1c00b900e26c541c896
Athul Raj <athulr@sensehawk.com>
2021-04-02 18:36:01 +0530

    Initial commit

---
+++ App.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import 'react-native-gesture-handler';
+import {NavigationStack} from './ui/router/NavigationStack';
+
+const App: () => React$Node = () => {
+  return <NavigationStack />;
+};
+
+export default App;
