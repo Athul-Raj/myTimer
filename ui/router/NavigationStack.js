6569c95883e130e861be2a05143b61ea2cccb0dd
Athul Raj <athulr@sensehawk.com>
2021-04-03 11:03:28 +0530

    Route updates

--- ui/router/NavigationStack.js
+++ ui/router/NavigationStack.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import {NavigationContainer} from '@react-navigation/native';
 import {createStackNavigator} from '@react-navigation/stack';
-import {RouteName} from './Routes';
+import RouteName from './Routes';
 import {TaskDetails, TaskList} from '../screens';
 
 const Stack = createStackNavigator();
--- ui/router/Routes.js
+++ ui/router/Routes.js
@@ -1,4 +1,4 @@
-export const RouteName = {
+export default {
   TaskList: 'TaskList',
   TaskDetails: 'TaskDetails',
   TagList: 'TagList',
